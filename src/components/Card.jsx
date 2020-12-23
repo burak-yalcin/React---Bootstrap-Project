dee162f712a9dd89033e0459ef8aa8fd9bcdc50a
burak-yalcin <brkylcn12@gmail.com>
2020-12-24 00:08:57 +0300

    1 file modified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Card({ title, image, content }) {
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+
+  function likeHandler() {
+    var newLike = like;
+    setLike(newLike + 1);
+  }
+
+  function dislikeHandler() {
+    var newDislike = dislike;
+    setDislike(newDislike + 1);
+  }
+
   return (
     <div className="card m-2" style={{ width: 300 }}>
       <img src={image} className="card-img-top" alt={title} />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{content}</p>
-        <a href="#" className="btn btn-primary">
-          Read more..
+        <a onClick={likeHandler} className="btn btn-success">
+          {like} Like
+        </a>
+        <a onClick={dislikeHandler} className="btn btn-danger ml-3">
+          {dislike} dislike
         </a>
       </div>
     </div>
