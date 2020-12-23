63cabe6dbbaf214f98848f6670ff044066af460f
burak-yalcin <brkylcn12@gmail.com>
2020-12-24 00:10:12 +0300

    1 file modified

--- src/data/LIBRARY_DB.js
+++ src/data/LIBRARY_DB.js
@@ -1,21 +1,48 @@
 const LIBRARY_DB = [
   {
     title: "Book of X",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a felis nisl. Nunc blandit tortor eget egestas cursus. Fusce tristique nec nisi sed consectetur. Fusce ut purus eget sapien sagittis pharetra ac vel magna. Morbi sodales accumsan ligula, at congue urna molestie vel. Phasellus luctus euismod velit, et maximus odio placerat id. ",
+
     image: "https://loremflickr.com/320/240/book"
   },
   {
     title: "Book of Y",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a felis nisl. Nunc blandit tortor eget egestas cursus. Fusce tristique nec nisi sed consectetur. Fusce ut purus eget sapien sagittis pharetra ac vel magna. Morbi sodales accumsan ligula, at congue urna molestie vel. Phasellus luctus euismod velit, et maximus odio placerat id. ",
+
     image: "https://loremflickr.com/320/240/library"
   },
   {
     title: "Book of Z",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras a felis nisl. Nunc blandit tortor eget egestas cursus. Fusce tristique nec nisi sed consectetur. Fusce ut purus eget sapien sagittis pharetra ac vel magna. Morbi sodales accumsan ligula, at congue urna molestie vel. Phasellus luctus euismod velit, et maximus odio placerat id. ",
+
     image: "https://loremflickr.com/320/240/read"
+  },
+  {
+    title: "Book of X",
+
+    image: "https://loremflickr.com/320/240/house"
+  },
+  {
+    title: "Book of Y",
+
+    image: "https://loremflickr.com/320/240/man"
+  },
+  {
+    title: "Book of Z",
+
+    image: "https://loremflickr.com/320/240/woman"
+  },
+  {
+    title: "Book of X",
+
+    image: "https://loremflickr.com/320/240/dog"
+  },
+  {
+    title: "Book of Y",
+
+    image: "https://loremflickr.com/320/240/cat"
+  },
+  {
+    title: "Book of Z",
+
+    image: "https://loremflickr.com/320/240/donkey"
   }
 ];
 
