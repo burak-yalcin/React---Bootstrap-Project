b59681a85ddc7a2551ef4d9455366d20dd63a3c3
burak-yalcin <brkylcn12@gmail.com>
2020-12-24 00:22:51 +0300

    4 files modified

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function Card({ title, image }) {
-  const [like, setLike] = useState(0);
-  const [dislike, setDislike] = useState(0);
-
-  function likeHandler() {
-    var newLike = like;
-    setLike(newLike + 1);
-  }
-
-  function dislikeHandler() {
-    var newDislike = dislike;
-    setDislike(newDislike + 1);
-  }
-
+export default function Card({ title, image, content }) {
   return (
     <div className="card m-2" style={{ width: 300 }}>
       <img src={image} className="card-img-top" alt={title} />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
-        <a onClick={likeHandler} className="btn btn-success">
-          {like} Like
-        </a>
-        <a onClick={dislikeHandler} className="btn btn-danger ml-3">
-          {dislike} dislike
+        <p className="card-text">{content}</p>
+        <a href="#" className="btn btn-primary">
+          Read more..
         </a>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="#">
           Click
         </a>
